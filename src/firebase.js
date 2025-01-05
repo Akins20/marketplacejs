a0e18f9b2dc0f8ac186b206474876ad2bea4dbd5
Ogunbiyi Elijah <115193430+Akins20@users.noreply.github.com>
2025-01-05 09:03:40 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getDatabase } from "firebase/database";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -10,18 +9,17 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB-4D8OiO9WtHBplqb3JyPxPKkb3yzLvas",
-  authDomain: "nuwa-hairdress.firebaseapp.com",
-  projectId: "nuwa-hairdress",
-  storageBucket: "nuwa-hairdress.appspot.com",
-  messagingSenderId: "6443798503",
-  appId: "1:6443798503:web:ff6d74e632c71143b23742",
-  measurementId: "G-DSLH2GN316",
+  apiKey: "As",
+  authDomain: "yourauthdomain",
+  projectId: "your project id",
+  storageBucket: "your storage bucket",
+  messagingSenderId: "your messaging sender id",
+  appId: "well yuno",
+  measurementId: "also yuno",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const firestore = getFirestore(app);
 export const storage = getStorage(app);
-export const db = getDatabase(app);
 export const auth = getAuth(app);
